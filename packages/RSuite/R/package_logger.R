0584509ecbb5eafb125d6b87a451348ec2e3bef4
Wit Jakuczun <wit.jakuczun@wlogsolutions.com>
2017-09-24 13:02:50 +0200

    Pkg loggers were improperly initialized (workaround for logging package)

--- packages/RSuite/R/package_logger.R
+++ packages/RSuite/R/package_logger.R
@@ -6,10 +6,11 @@
 #----------------------------------------------------------------------------
 
 .logger_name <- "rsuite"
-setLevel("FINEST", logging::getLogger(.logger_name))
+.pkg_logger <- logging::getLogger(.logger_name)
+.pkg_logger$setLevel("FINEST")
 
-pkg_loginfo <- function(msg, ...) { logging::loginfo(msg, ..., logger = getLogger(.logger_name)) }
-pkg_logdebug <- function(msg, ...) { logging::logdebug(msg, ..., logger = getLogger(.logger_name)) }
-pkg_logerror <- function(msg, ...) { logging::logerror(msg, ..., logger = getLogger(.logger_name)) }
-pkg_logwarn <- function(msg, ...) { logging::logwarn(msg, ..., logger = getLogger(.logger_name)) }
-pkg_logfinest <- function(msg, ...) { logging::logfinest(msg, ..., logger = getLogger(.logger_name)) }
+pkg_loginfo <- function(msg, ...) { logging::loginfo(msg, ..., logger = .pkg_logger) }
+pkg_logdebug <- function(msg, ...) { logging::logdebug(msg, ..., logger = .pkg_logger) }
+pkg_logerror <- function(msg, ...) { logging::logerror(msg, ..., logger = .pkg_logger) }
+pkg_logwarn <- function(msg, ...) { logging::logwarn(msg, ..., logger = .pkg_logger) }
+pkg_logfinest <- function(msg, ...) { logging::logfinest(msg, ..., logger = .pkg_logger) }
