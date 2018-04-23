14646241554e2b67aa98df96f270bad0466c85cd
ws171913 <ws171913@wlogsolutions.com>
2018-04-23 18:47:50 +0200

    RSuite CLI: - docker command has new sub-commands to support development process: run, list, stop, exec, build

--- cli/R/docker_utils.R
+++ cli/R/docker_utils.R
@@ -39,22 +39,18 @@ get_platform_id <- function(platform) {
 #'
 #' Will raise error if platform is not supported.
 #'
-get_docker_base_image <- function(prj, rver, platform) {
+get_docker_base_image <- function(rver, platform) {
   get_platform_id(platform) # just to check if platform is valid
-
-  params <- prj$load_params()
-  if (is.null(rver)) {
-    rver <- params$r_ver
-  }
   sprintf("wlog/rsuite:%s_r%s", platform, RSuite:::majmin_rver(rver))
 }
 
 #'
 #' Builds image id with latest RSuite & RSuite CLI to build project for the platform.
 #'
-get_docker_image <- function(prj, rver, platform) {
+get_docker_rsuite_image <- function(rver, platform) {
+  stopifnot(!is.null(rver))
   sprintf("%s_v%s",
-          get_docker_base_image(prj, rver, platform),
+          get_docker_base_image(rver, platform),
           get_latest_release(get_platform_id(platform))$ver)
 }
 
@@ -73,7 +69,8 @@ exec_docker_cmd <- function(args, cmd_desc) {
   logdebug("running: %s", cmd)
   con <- pipe(cmd, open = "rt")
 
-  result <- tryCatch({
+  tryCatch({
+    lines <- c()
     while(TRUE) {
       ln <- readLines(con, n = 1, skipNul = T)
       if (!length(ln)) {
@@ -82,8 +79,10 @@ exec_docker_cmd <- function(args, cmd_desc) {
       if (ln == success_code) {
         break
       }
+      lines <- c(lines, ln)
       logdebug("> %s", ln)
     }
+    return(invisible(lines))
   },
   finally = close(con))
 }
@@ -93,11 +92,15 @@ exec_docker_cmd <- function(args, cmd_desc) {
 #'
 #' @return docker container name
 #'
-run_container <- function(docker_image) {
-  cont_name <- basename(tempfile("cont_"))
+run_container <- function(docker_image, name) {
+  if (is.null(name)) {
+    cont_name <- basename(tempfile("rsbuild-"))
+  } else {
+    cont_name <- paste0("rsbuild-", name)
+  }
   loginfo("Starting container %s based on %s image ...", cont_name, docker_image)
   exec_docker_cmd(c("run", "--name", cont_name, "-d", docker_image),
-                  sprintf("Staring container %s based on %s", cont_name, docker_image))
+                  sprintf("Starting container %s based on %s", cont_name, docker_image))
   loginfo("... done.")
   return(cont_name)
 }
@@ -122,6 +125,37 @@ stop_container <- function(cont_name) {
   exec_docker_cmd(c("rm", "-f", cont_name),
                   sprintf("Removing container %s", cont_name))
   loginfo("... done.")
+}
+
+#'
+#' Gets names of RSuite build containers.
+#'
+#' @param cont_name container name prefix to find. If NULL will return all RSuite build containers.
+#'   If passed should be exactly one container with such name (type: character(1), default: NULL).
+#'
+#' @return names of RSuite build containers detectected. (type: character(N))
+#'
+get_rsbuild_names <- function(cont_name = NULL) {
+  output <- exec_docker_cmd(c("ps", "-f", "name=rsbuild-"), "Listing RSuite build containers") # from docker_utils.R
+  names <- unlist(lapply(strsplit(output[-1], "\\s+"), function(ln) { ln[length(ln)] }))
+
+  if (is.null(cont_name)) {
+    return(names)
+  }
+
+  if (grepl("^rsbuild[-]", cont_name)) {
+    names <- names[names == cont_name]
+  } else {
+    names <- names[grepl(paste0("^rsbuild[-]", cont_name), names)]
+  }
+
+  if (length(names) > 1) {
+    stop(sprintf("Name '%s' is ambiguous", cont_name))
+  }
+  if (length(names) != 1) {
+    stop(sprintf("Container '%s' not found", cont_name))
+  }
+  return(names)
 }
 
 #'
