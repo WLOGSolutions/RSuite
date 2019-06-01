8757538641872966b939b59b55b49e731ee0d1c1
ws171913 <ws171913@wlogsolutions.com>
2019-06-01 16:49:38 +0200

    Fix Travis CI build

--- tests/test_repo_dir_upload_from_bioc.R
+++ tests/test_repo_dir_upload_from_bioc.R
@@ -13,6 +13,8 @@ source("R/repo_management.R")
 
 
 test_that_managed("Uploading Bioc (basic)", {
+  skip_if_not(RSuite:::current_rver() == "3.6") # BiocGenerics needs R3.6 and remotes does not properly handles Bioc branches
+
   prj <- init_test_project(repo_adapters = c("CRAN"))
   mgr <- init_test_manager(prj)
   RSuite::prj_config_set_repo_adapters(c("CRAN", sprintf("Dir[%s]", mgr$path)), prj = prj)
